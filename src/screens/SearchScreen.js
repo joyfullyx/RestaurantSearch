c2be779e4ad9691c61b377314794f4a70419fbed
Joy Rhee <joyfullyx@gmail.com>
2021-10-21 17:02:25 -0700

    create custom hooks and results list

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,28 +1,27 @@
 import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import SearchBar from "../components/SearchBar";
-import yelp from "../api/yelp";
+import useResults from "../hooks/useResults";
+import ResultsList from "../components/ResultsList";
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
-  const [results, setResults] = useState([]);
+  const [searchApi, results, errorMessage] = useResults();
 
-  const searchApi = async () => {
-    const response = await yelp.get("/search", {
-      params: {
-        limit: 50,
-        term: term,
-        location: "san jose",
-      },
-    });
-    setResults(response.data.businesses);
-  };
+  console.log(results);
 
   return (
     <View>
-      <SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchApi} />
-      <Text>Search Screen</Text>
+      <SearchBar
+        term={term}
+        onTermChange={setTerm}
+        onTermSubmit={() => searchApi(term)}
+      />
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>Found {results.length} results</Text>
+      <ResultsList title="Cheap" />
+      <ResultsList title="Average" />
+      <ResultsList title="Ballin'" />
     </View>
   );
 };
